6310586d66ee9930e533a23055307f13f4f79d1d
moo46857-a11y <moo46857@ttu.edu>
2026-05-04 22:27:29 -0500

    Question 2 complete

--- lab07/Question2/student.c
+++ lab07/Question2/student.c
@@ -28,9 +28,62 @@ Notes:
 - You may use temporary arrays inside your merge function.
 */
 
+void merge(int arr[], int left, int mid, int right) {
+	int i, j, k;
+	int n1 = mid - left + 1;
+	int n2 = right - mid;
+	int L[n1], R[n2];
+
+	for(i = 0; i < n1; i++)
+		L[i] = arr[left+i];
+	for(j = 0; j < n2; j++)
+		R[j] = arr[mid+1+j];
+
+	i = 0;
+	j = 0;
+	k = left;
+
+	while(i < n1 && j < n2) {
+		if(L[i] <= R[j]) {
+			arr[k] = L[i];
+			i++;
+		} else {
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	while(i < n1) {
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	while(j < n2) {
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+}
+
+void mergeSortHelper(int arr[], int left, int right) {
+	if(left < right) {
+		int mid = left + (right - left) / 2;
+		mergeSortHelper(arr, left, mid);
+		mergeSortHelper(arr, mid + 1, right);
+		merge(arr, left, mid, right);
+	}
+}
+
+
+
 void mergeSort(int arr[], int size) {
     // TODO: implement merge sort
     (void)arr;
     (void)size;
+    if(size > 1) {
+	    mergeSortHelper(arr, 0, size - 1);
+    }
 }
 
